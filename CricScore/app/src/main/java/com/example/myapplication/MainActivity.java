a036f21100ca6f3181b6eb339dda061ebc00cd78
abhishekpatel111 <48686115+abhishekpatel111@users.noreply.github.com>
2019-06-16 19:56:25 +0530

    Done

---
+++ CricScore/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.myapplication;
+
+i
+import android.widget.Button;
+
+public class MainActivity extends ActivityAppCompat {
+//
+    Button b1,b2,b3;
+    TextView  p1score p2score p3score,over;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //
+        bt1=findViewById(android:id="@+id/button")
+)
+    }
+}
